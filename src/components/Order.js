332a70dd213205205bc4f5493596599377b4dfb7
zbruennig <zkb@ku.edu>
2017-11-11 12:40:09 -0600

    Create empty Order, Restaurant components

---
+++ src/components/Order.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Order extends Component{
+
+    render(){
+
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Order;
